8c3c9abc1c23d0ad3d7373c19b5fde53c8d0c209
andrearomperez <150700647+andrearomperez@users.noreply.github.com>
2024-08-05 18:28:04 -0500

    Update appASEI.R

--- Censo/DeficitHabitacional/appASEI.R
+++ Censo/DeficitHabitacional/appASEI.R
@@ -88,18 +88,8 @@ ui <- fluidPage(
                                       selected = "TODOS",
                                       width = "220px"
                           ),
-                          selectInput(inputId = "Prov",
-                                      label = "Elija la provincia",
-                                      choices = listProv,
-                                      selected = "TODOS",
-                                      width = "220px"
-                          ),
-                          selectInput(inputId = "Dist",
-                                      label = "Elija el distrito",
-                                      choices = listDist,
-                                      selected = "TODOS",
-                                      width = "220px"
-                          ),
+                          uiOutput("uiProv"),
+                          uiOutput("uiDist"),
                           selectInput(inputId = "Indicador",
                                       label = "Seleccione el indicador",
                                       choices = listIndicadores,
@@ -118,16 +108,140 @@ ui <- fluidPage(
              
   ))
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+## Define server logic required to draw a histogram
+server <- function(input, output, session) {
 
-    output$map1 <- renderPlotly({
+## Present provincias depending on the selected departamento and distritos depending on the selected provincia.
+  observeEvent(input$Dpto, {
+    if (input$Dpto == "TODOS") {
+      prov_choices <- provincias %>%
+        select(provincia) %>%
+        arrange(provincia) %>%
+        drop_na() %>%
+        pull(provincia)
+    } else {
+      ubigeo_dpto <- departamentos %>%
+        filter(departamento == input$Dpto) %>%
+        pull(ubigeo)
+      prov_choices <- provincias %>%
+        filter(substr(ubigeo, 1, 2) == substr(ubigeo_dpto, 1, 2)) %>%
+        select(provincia) %>%
+        arrange(provincia) %>%
+        drop_na() %>%
+        pull(provincia)
+    }
+    prov_choices <- c("TODOS", prov_choices)
+    
+    updateSelectInput(session, "Prov", choices = prov_choices)
+  })
+  
+  observeEvent(input$Prov, {
+    if (input$Prov == "TODOS") {
+      dist_choices <- distritos %>%
+        select(distrito) %>%
+        arrange(distrito) %>%
+        drop_na() %>%
+        pull(distrito)
+    } else {
+      ubigeo_prov <- provincias %>%
+        filter(provincia == input$Prov) %>%
+        pull(ubigeo)
+      dist_choices <- distritos %>%
+        filter(substr(ubigeo, 1, 4) == substr(ubigeo_prov, 1, 4)) %>%
+        select(distrito) %>%
+        arrange(distrito) %>%
+        drop_na() %>%
+        pull(distrito)
+    }
+    dist_choices <- c("TODOS", dist_choices)
+    
+    updateSelectInput(session, "Dist", choices = dist_choices)
+  })
+  
+  output$uiProv <- renderUI({
+    selectInput(inputId = "Prov",
+                label = "Elija la provincia",
+                choices = c("TODOS"),
+                selected = "TODOS",
+                width = "220px")
+  })
+  
+  output$uiDist <- renderUI({
+    selectInput(inputId = "Dist",
+                label = "Elija el distrito",
+                choices = c("TODOS"),
+                selected = "TODOS",
+                width = "220px")
+  })
+  
+  
+  ## Elaborate the map for the different indicators
+    
+  output$map1 <- renderPlotly({
       
       custom_palette <- c("#2dc937", "#e7b416", "#cc3232")
       
      
       if (input$Dpto != "TODOS" & input$Prov != "TODOS" & input$Dist != "TODOS"){
-        print("Zoom a la provincia y ponemos el mapa provincia con divisiones distritales")
+        map_peru <- map_DIST  %>%  #Cargamos la base de datos sobre las provincias del Peru
+          rename(ubigeo = COD_DISTRITO) %>% #renombramos la variable del DF para el merge por UBIGEO
+          st_as_sf
+        
+        alterna <- data  %>%
+          select(ubigeo, input$Indicador) 
+        
+        colnames(alterna) <- c("ubigeo", "indicador")
+        
+        map_shiny <- merge(x = map_peru, y = alterna, by = "ubigeo", all.x = TRUE)  %>% 
+          mutate(indicador = as.numeric(indicador)) 
+        
+        # Filtrar solo los distritos de la provincia seleccionada
+        
+        ubigeo_prov <- provincias %>%
+          filter(provincia == input$Prov) %>%
+          pull(ubigeo)
+        
+        map_shiny <- map_shiny %>%
+          filter(substr(ubigeo, 1, 4) == substr(ubigeo_prov, 1, 4))
+        
+        breaks <- classIntervals(map_shiny$indicador, n = 3, style = "jenks")
+        map_shiny$jenks_breaks <- cut(map_shiny$indicador, breaks$brks, include.lowest = TRUE)
+        
+        # Identificar el distrito seleccionado
+        
+        selected_district <- map_shiny %>% 
+          filter(ubigeo == distritos$ubigeo[distritos$distrito == input$Dist])
+               
+        mapa <- map_shiny %>% 
+          ggplot() +
+          aes(geometry = geometry) +
+          geom_sf(aes(fill = jenks_breaks), linetype = 1, lwd = 0.25) +
+          geom_sf(data = selected_district, fill = "red", color = "black", size = 1) +  # Resaltar distrito seleccionado
+          geom_sf(fill = NA, color = "black", size = 2) +  # Añadir bordes de distritos
+          theme_minimal()+
+          theme(axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks = element_blank()) +
+          scale_fill_manual(values = custom_palette, name = "Porcentaje de hogares (%)")
+        
+        ggplotly(mapa)
+        
+#        ubigeo_prov <- provincias %>%
+#          filter(provincia == input$Prov) %>%
+#          pull(ubigeo)
+        
+#        geom_prov <- map_PROV %>%
+#          filter(COD_PROVINCIA == ubigeo_prov)
+        
+#        if (!is.null(geom_prov) && nrow(geom_prov) > 0) {
+#          geom_prov <- geom_prov %>%
+#            st_as_sf() %>%
+#            st_union()
+        
+        bbox <- st_bbox(selected_district)
+        buffer <- 0.1
+        mapa <- mapa + coord_sf(xlim = c(bbox["xmin"] - buffer, bbox["xmax"] + buffer), 
+                                ylim = c(bbox["ymin"] - buffer, bbox["ymax"] + buffer))
+          
+          
         #Poner el distrito seleccionado con un color distinto
         
       } else if (input$Dpto != "TODOS" & input$Prov == "TODOS"){
@@ -138,7 +252,8 @@ server <- function(input, output) {
         print("Mapa del Perú por regiones ")
         
         map_peru <- map_DEP  %>%  #Cargamos la base de datos sobre los departamentos del Peru
-          rename(ubigeo = COD_DEPARTAMENTO ) #renombramos la variable del DF para el merge por UBIGEO
+          rename(ubigeo = COD_DEPARTAMENTO ) %>% #renombramos la variable del DF para el merge por UBIGEO
+          st_as_sf()
        
         alterna <- data  %>%
           select(ubigeo, input$Indicador) 
@@ -160,6 +275,7 @@ server <- function(input, output) {
           theme(axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks = element_blank()) +
           scale_fill_manual(values = custom_palette, name = "Porcentaje de hogares (%)")
         
+        ggplotly(mapa)
 
       }
     })
