3bdc48b607fed056e84d5723dc70b14e16ca1ac3
andrearomperez <150700647+andrearomperez@users.noreply.github.com>
2024-08-07 18:09:01 -0500

    Update appASEI.R

--- Censo/DeficitHabitacional/appASEI.R
+++ Censo/DeficitHabitacional/appASEI.R
@@ -183,9 +183,10 @@ server <- function(input, output, session) {
       
      
       if (input$Dpto != "TODOS" & input$Prov != "TODOS" & input$Dist != "TODOS"){
+        
         map_peru <- map_DIST  %>%  #Cargamos la base de datos sobre las provincias del Peru
           rename(ubigeo = COD_DISTRITO) %>% #renombramos la variable del DF para el merge por UBIGEO
-          st_as_sf
+          st_as_sf()
         
         alterna <- data  %>%
           select(ubigeo, input$Indicador) 
@@ -207,6 +208,10 @@ server <- function(input, output, session) {
         breaks <- classIntervals(map_shiny$indicador, n = 3, style = "jenks")
         map_shiny$jenks_breaks <- cut(map_shiny$indicador, breaks$brks, include.lowest = TRUE)
         
+        map_shiny <- map_shiny %>%
+          mutate(text = paste(distritos$distrito[match(ubigeo, distritos$ubigeo)],"<br>",
+                              input$Indicador, ":", round(indicador, 2)))
+        
         # Identificar el distrito seleccionado
         
         selected_district <- map_shiny %>% 
@@ -215,39 +220,114 @@ server <- function(input, output, session) {
         mapa <- map_shiny %>% 
           ggplot() +
           aes(geometry = geometry) +
-          geom_sf(aes(fill = jenks_breaks), linetype = 1, lwd = 0.25) +
-          geom_sf(data = selected_district, fill = "red", color = "black", size = 1) +  # Resaltar distrito seleccionado
-          geom_sf(fill = NA, color = "black", size = 2) +  # Añadir bordes de distritos
+          geom_sf(aes(fill = jenks_breaks, text=text), linetype = 1,
+                  lwd = 0.25) +
+          geom_sf(data = selected_district, aes(text = text), fill = NA, color = "black", size = 1.5) +  # Añadir bordes de distritos
           theme_minimal()+
           theme(axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks = element_blank()) +
-          scale_fill_manual(values = custom_palette, name = "Porcentaje de hogares (%)")
+          scale_fill_manual(values = custom_palette, name = input$Indicador)
         
-        ggplotly(mapa)
-        
-#        ubigeo_prov <- provincias %>%
-#          filter(provincia == input$Prov) %>%
-#          pull(ubigeo)
-        
-#        geom_prov <- map_PROV %>%
-#          filter(COD_PROVINCIA == ubigeo_prov)
-        
-#        if (!is.null(geom_prov) && nrow(geom_prov) > 0) {
-#          geom_prov <- geom_prov %>%
-#            st_as_sf() %>%
-#            st_union()
-        
-        bbox <- st_bbox(selected_district)
-        buffer <- 0.1
-        mapa <- mapa + coord_sf(xlim = c(bbox["xmin"] - buffer, bbox["xmax"] + buffer), 
-                                ylim = c(bbox["ymin"] - buffer, bbox["ymax"] + buffer))
-          
-          
-        #Poner el distrito seleccionado con un color distinto
-        
+        ggplotly(mapa, tooltip = "text") %>% 
+          style(hoverinfo = "text", traces = 1)
+
       } else if (input$Dpto != "TODOS" & input$Prov == "TODOS"){
-          print("Zoom al departamento y ponemos el mapa del dpto con divisiones proviciales y distritales")
+        
+        map_peru <- map_DEP  %>%  #Cargamos la base de datos sobre los departamentos del Peru
+          rename(ubigeo = COD_DEPARTAMENTO ) %>% #renombramos la variable del DF para el merge por UBIGEO
+          st_as_sf()
+        
+        alterna <- data  %>%
+          select(ubigeo, input$Indicador) 
+        
+        colnames(alterna) <- c("ubigeo", "indicador")
+        
+        map_shiny <- merge(x = map_peru, y = alterna, by = "ubigeo", all.x = TRUE)  %>% 
+          mutate(indicador = as.numeric(indicador)) 
+        
+        breaks <- classIntervals(map_shiny$indicador, n = 3, style = "jenks")
+        map_shiny$jenks_breaks <- cut(map_shiny$indicador, breaks$brks, include.lowest = TRUE)
+        
+        map_shiny <- map_shiny %>%
+          mutate(text = paste(departamentos$departamento[match(ubigeo, departamentos$ubigeo)],"<br>",
+                              input$Indicador, ":", round(indicador, 2)))
+        
+        # Identificar el distrito seleccionado
+        
+        selected_departamento <- map_shiny %>% 
+          filter(ubigeo == departamentos$ubigeo[departamentos$departamento == input$Dpto])
+        
+        mapa <- map_shiny %>% 
+          ggplot() +
+          aes(geometry = geometry) +
+          geom_sf(aes(fill = jenks_breaks, text=text), linetype = 1,
+                  lwd = 0.25) +
+          geom_sf(data = selected_departamento, aes(text = text), fill = NA, color = "black", size = 1.5) +  # Resaltar departamento seleccionado
+          theme_minimal()+
+          theme(axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks = element_blank()) +
+          scale_fill_manual(values = custom_palette, name = input$Indicador)
+        
+        ggplotly(mapa, tooltip = "text") %>% 
+          style(hoverinfo = "text", traces = 1)
+          
       } else if (input$Dpto != "TODOS" & input$Prov != "TODOS" & input$Dist == "TODOS"){
-        print("Zoom a la provincia y ponemos el mapa provincia con divisiones distritales")
+        
+        map_peru <- map_PROV  %>%  #Cargamos la base de datos sobre las provincias del Peru
+          rename(ubigeo = COD_PROVINCIA) %>% #renombramos la variable del DF para el merge por UBIGEO
+          st_as_sf()
+        
+        alterna <- data  %>%
+          select(ubigeo, input$Indicador) 
+        
+        colnames(alterna) <- c("ubigeo", "indicador")
+        
+        map_shiny <- merge(x = map_peru, y = alterna, by = "ubigeo", all.x = TRUE)  %>% 
+          mutate(indicador = as.numeric(indicador)) 
+      
+        # Filtrar solo las provincias del departamento seleccionado
+        
+        ubigeo_dpto <- departamentos %>% 
+          filter(departamento == input$Dpto) %>% 
+          pull(ubigeo)
+        
+        # Caso de Callao donde solo hay una provincia
+        
+        if (ubigeo_dpto == "070000") {
+          # Incluir Lima (150000) si es Callao
+          map_shiny <- map_shiny %>%
+            filter(substr(ubigeo, 1, 2) %in% c(substr(ubigeo_dpto, 1, 2), "15"))
+        } else {
+          map_shiny <- map_shiny %>%
+            filter(substr(ubigeo, 1, 2) == substr(ubigeo_dpto, 1, 2))
+        }
+        
+        # Continuación del código para generar los breaks y el mapa
+        
+        breaks <- classIntervals(map_shiny$indicador, n = 3, style = "jenks")
+        map_shiny$jenks_breaks <- cut(map_shiny$indicador, breaks$brks, include.lowest = TRUE)
+        
+        map_shiny <- map_shiny %>%
+          mutate(text = paste(provincias$provincia[match(ubigeo, provincias$ubigeo)],"<br>",
+                              input$Indicador, ":", round(indicador, 2)))
+        
+        # Identificar la provincia seleccionado
+        
+        selected_provincia <- map_shiny %>% 
+          filter(ubigeo == provincias$ubigeo[provincias$provincia == input$Prov])
+        
+        mapa <- map_shiny %>% 
+          ggplot() +
+          aes(geometry = geometry) +
+          geom_sf(aes(fill = jenks_breaks, text = text), linetype = 1,
+                  lwd = 0.25) +
+          geom_sf(data = selected_provincia, aes(text = text), fill = NA, color = "black", size = 1.5) + # Resaltar provincia seleccionada
+          theme_minimal() +
+          theme(axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks = element_blank()) +
+          scale_fill_manual(values = custom_palette, name = input$Indicador)
+        
+        # Convertir el ggplot a un objeto plotly
+        ggplotly(mapa, tooltip = "text") %>% 
+          style(hoverinfo = "text", traces = 1)
+        
       } else {
         print("Mapa del Perú por regiones ")
         
@@ -273,7 +353,7 @@ server <- function(input, output, session) {
                   lwd = 0.25) +
           theme_minimal()+
           theme(axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks = element_blank()) +
-          scale_fill_manual(values = custom_palette, name = "Porcentaje de hogares (%)")
+          scale_fill_manual(values = custom_palette, name = input$Indicador)
         
         ggplotly(mapa)
 
